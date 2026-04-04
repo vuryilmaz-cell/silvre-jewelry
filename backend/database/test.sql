18b11c9ce113bb72e7592530c608cdd0e600f20e
Vural Yılmaz <vuryilmaz@gmal.com>
2026-04-05 00:50:29 +0300

    sıkça sorulan sorular güncellendi

--- backend/database/test.sql
+++ backend/database/test.sql
@@ -5,3 +5,18 @@ WHERE image_url LIKE '%http://localhost:5000http://localhost:5000/%';
 
 
 SELECT * FROM categories
+
+SELECT * FROM products
+
+SELECT * FROM INDEX
+
+
+UPDATE products 
+SET gender = 'erkek' 
+WHERE category_id = 4;
+
+
+SELECT id, name, gender, material, weight FROM products WHERE id IN (9, 10, 11);
+
+ALTER TABLE categories ADD COLUMN gender TEXT DEFAULT 'unisex' CHECK(gender IN ('kadın', 'erkek', 'unisex', 'çocuk'));
+ALTER TABLE products ADD COLUMN gender TEXT DEFAULT 'unisex' CHECK(gender IN ('kadın', 'erkek', 'unisex', 'çocuk'));
